0a4d23684dbbd5a5258c072b3f9fb4f9f8921896
Jose Asto <155777725+DhudsQ@users.noreply.github.com>
2026-05-10 20:11:47 -0500

    feat(develop): deployment and connection with json server through firebase and reader

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,23 @@
 export const environment = {
   production: true,
+
   // Base API URL
-  serverBasePath: 'http://localhost:8080/api/v1',
+  serverBasePath: 'https://json-server-y51j.onrender.com',
   // IAM (Identity and Access Management)
   iamSignInEndpointPath: '/auth/sign-in',
   iamSignUpEndpointPath: '/auth/sign-up',
   iamRecoverPasswordEndpointPath: '/auth/recover-password',
   // Catalog (Productos e Inventario)
   catalogEndpointPath: '/catalog',
-  catalogProductsEndpointPath: '/catalog/products',
-  catalogInventoryEndpointPath: '/catalog/inventory',
+  catalogProductsEndpointPath: '/catalog',
+  catalogInventoryEndpointPath: '/catalog',
   // Ordering (Solicitudes y Órdenes)
   orderingRequestsEndpointPath: '/ordering/requests',
   orderingOrdersEndpointPath: '/ordering/orders',
   // Fulfillment (Logística y Despacho)
-  fulfillmentVehiclesEndpointPath: '/fulfillment/vehicles',
-  fulfillmentDriversEndpointPath: '/fulfillment/drivers',
-  fulfillmentDeliveriesEndpointPath: '/fulfillment/deliveries',
+  fulfillmentVehiclesEndpointPath: '/vehicles',
+  fulfillmentDriversEndpointPath: '/drivers',
+  fulfillmentDeliveriesEndpointPath: '/deliveries',
   // Payment (Transacciones y Pagos)
   paymentTransactionsEndpointPath: '/payment/transactions',
   paymentPaymentsEndpointPath: '/payment/payments',
