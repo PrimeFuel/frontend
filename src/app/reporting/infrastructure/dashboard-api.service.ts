4a7200a5d0ad66ddc1bde3a01514968cc0a89162
Brayan Corvacho <alexiscorvachocd@gmail.com>
2026-06-17 00:07:59 -0500

    feat(reporting): add dashboard API service

---
+++ src/app/reporting/infrastructure/dashboard-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardApiService {
+
+  private http = inject(HttpClient);
+
+  private serverUrl = environment.serverBasePath;
+
+  getInventory() {
+    return this.http.get<any[]>(`${this.serverUrl}/fuel-products`);
+  }
+
+  getOrders() {
+    return this.http.get<any[]>(`${this.serverUrl}/fuel-orders`);
+  }
+
+}
