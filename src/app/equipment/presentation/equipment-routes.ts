d15cec35d71ab675fbc18ce4317ee03c795b2461
Diego Herrera <u202319027@upc.edu.pe>
2026-06-17 01:11:40 -0500

    feat(equipment): add equipment presentation

---
+++ src/app/equipment/presentation/equipment-routes.ts
@@ -0,0 +1,21 @@
+﻿import { Routes } from '@angular/router';
+import { Layout } from '../../shared/presentation/component/layout/layout';
+
+const equipmentList = () =>
+  import('./views/equipment-list/equipment-list').then((m) => m.EquipmentList);
+const equipmentForm = () =>
+  import('./views/equipment-form/equipment-form').then((m) => m.EquipmentForm);
+
+export const equipmentRoutes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', loadComponent: equipmentList },
+      { path: 'new', loadComponent: equipmentForm },
+      { path: ':id/edit', loadComponent: equipmentForm },
+      { path: 'equipment-form', redirectTo: 'new', pathMatch: 'full' },
+      { path: 'equipment-form/:id', redirectTo: ':id/edit', pathMatch: 'full' },
+    ],
+  },
+];
